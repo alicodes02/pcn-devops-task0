cea81f28984ff0ca59aa811b7136681bbc52fcfd
Muhammad Ali <se.muhammadali01@gmail.com>
2024-06-21 14:51:03 +0500

    created Dockerfiles and docker-compose files

---
+++ database/db.sql
@@ -0,0 +1,6 @@
+USE Login;
+
+CREATE TABLE Credentials(email VARCHAR(50),password VARCHAR(50));
+
+INSERT INTO Credentials (email, password) VALUES ('hani@example.com', '1234');
+INSERT INTO Credentials (email, password) VALUES ('admin', 'admin');
